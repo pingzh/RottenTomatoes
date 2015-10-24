18293abb388d79aa0b70726ae791f030a39e1646
Ping Zhang <pzhang@groupon.com>
2015-10-23 19:20:58 -0500

    collection view cell is done

---
+++ Podfile
@@ -0,0 +1,16 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target 'RottenTomatoes' do
+  pod 'Alamofire', '~> 3.0'
+end
+
+target 'RottenTomatoesTests' do
+
+end
+
+target 'RottenTomatoesUITests' do
+
+end
+
---
+++ RottenTomatoes/MoviesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesCollectionViewCell.swift
+//  RottenTomatoes
+//
+//  Created by Ping Zhang on 10/23/15.
+//  Copyright © 2015 Ping Zhang. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+}
